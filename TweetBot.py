09089a2b7947f2d1bae52e56476719ed4367c9f5
00111000 <khesualdo.github@gmail.com>
2018-02-17 20:06:56 -0700

    Added comments

--- TweetBot.py
+++ TweetBot.py
@@ -1,26 +1,50 @@
+# Imports for sending tweets
 import tweepy
 import KeyParser
-from icecream import ic
 
+# Imports for Google Static Maps API
 from io import BytesIO
 from PIL import Image
 from urllib import request
 
-def id(str):
-    return '@'+str
+# Debug library
+from icecream import ic
 
-def tag(str):
-    return '#'+str
+def id(str=None):
+    if str is None:
+        return None
+    else:
+        return '@'+str
 
+def hash(str=None):
+    if str is None:
+        return None
+    else:
+        return '#'+str
+
+'''
+Uses Google Static Maps API to create a screenshot of
+a centered map with a red marker at target locations
+
+Name of the file created is MAP.gif, the name is constant,
+since we do not need to save the screenshot for every tweet created.
+
+Returns the name of the file created.
+'''
 def getMap(geo_lat, geo_long):
     url = "http://maps.googleapis.com/maps/api/staticmap?center="+str(geo_lat)+\
     ","+str(geo_long)+"&size=800x800&markers=color:red%7Clabel:HAVANA%7C"+str(geo_lat)+\
     ","+str(geo_long)+"&zoom=14&sensor=false"
 
+    # BytesIO creates in-memory file object
+    # buffer can be used as input or output to most 
+    # functions that expect a standard file object.
     buffer = BytesIO(request.urlopen(url).read())
-    Image.open(buffer).save(str(abs(geo_lat))+'.gif','GIF')
 
-    return str(abs(geo_lat))+'.gif'
+    # Open the image from buffer (simulates a file)
+    # Save the buffer as a GIF (create the map image)
+    Image.open(buffer).save('MAP.gif','GIF')
+    return 'MAP.gif'
 
 def get_api(cfg):
     auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
@@ -40,21 +64,23 @@ def tweet(tweet=None, geo_lat=None, geo_long=None):
     if tweet is None:
         raise Exception('No msg for tweet!')
 
+    # Accept lat as float or int
     if geo_lat is not None and not isinstance(geo_lat, float):
         if not isinstance(geo_lat, int):
             raise Exception('Latitude should be a float or int')
 
+    # Accept long as float or int
     if geo_long is not None and not isinstance(geo_long, float):
         if not isinstance(geo_long, int):
             raise Exception('Longitude should be a float or int')
 
     try:
+
+        # Send a tweet with map, lat, long
         gif = getMap(geo_lat, geo_long)
         status = api.update_with_media(filename=gif, status=tweet,lat=geo_lat,long=geo_long)
-        # ic(status)
     except:
         ic("Error querying GoogleMaps")
-        status = api.update_status(status=tweet)
-        # ic(status)
 
-tweet("asdd", 1.0, 5.0)
+        # Send a tweet with lat, long
+        status = api.update_status(status=tweet)
