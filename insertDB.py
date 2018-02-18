8f0c690fe55e163a24b8db412bb0d1f9a59bec5d
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-02-17 20:35:17 -0700

    when it records a spike in sound, the db is updated

--- insertDB.py
+++ insertDB.py
@@ -1,9 +1,11 @@
-import cymysql
+#!/<path-to>/python
+# Get date from MQTT server
 
-dateTime = "hi"
-loudness = 345
-lat = 45
-long = 34
+import cymysql
+import paho.mqtt.client as mqtt
+import json
+import base64
+import datetime
 
 def writeToDB(dateTime = None, loudness = None, lat = None, long = None):
 	try:
@@ -18,5 +20,50 @@ def writeToDB(dateTime = None, loudness = None, lat = None, long = None):
 		cur.execute(query)
 	except:
 		print("Insert query execution failed.")
-	 
-writeToDB(dateTime, loudness, lat, long)
+
+APPEUI = "70B3D57EF00065F5"
+APPID  = "app_733"
+PSW    = 'ttn-account-v2.mt4eewK-lOs5bQyncG3Sx68ghj7WaH2-qF4eqjKC6nw'
+
+#Call back functions 
+
+# gives connection message
+def on_connect(mqttc, mosq, obj,rc):
+		print("Connected with result code: "+str(rc))
+		# subscribe for all devices of user
+		print("Subscribing")
+		mqttc.subscribe('app_733/devices/arduino_uno_grove_shield/up/soundLevel')
+		print("Done")
+
+# gives message from device
+def on_message(mqttc,obj,msg):
+		x = json.loads(msg.payload)
+		timestamp = datetime.datetime.now()
+		loudness = str(x)
+		print(str(timestamp) + " Sound Level: " + loudness)
+		lat = 51.08019000
+		long = -114.13051000
+		writeToDB(timestamp, loudness, lat, long)
+
+def on_publish(mosq, obj, mid):
+		print("mid: " + str(mid))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+		print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(mqttc,obj,level,buf):
+		print("message:" + str(buf))
+		print("userdata:" + str(obj))
+		
+mqttc= mqtt.Client()
+# Assign event callbacks
+mqttc.on_connect=on_connect
+mqttc.on_message=on_message
+
+mqttc.username_pw_set(APPID, PSW)
+mqttc.connect("us-west.thethings.network",1883,60)
+
+# and listen to server
+run = True
+while run:
+		mqttc.loop()
