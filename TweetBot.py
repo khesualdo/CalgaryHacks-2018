f298571e8db025c419a491c3e7cb2170a726f1c2
00111000 <khesualdo.github@gmail.com>
2018-02-17 17:02:47 -0700

    Added getMap

--- TweetBot.py
+++ TweetBot.py
@@ -1,8 +1,17 @@
 import tweepy
 import KeyParser
 
+from io import BytesIO
+from PIL import Image
+from urllib import request
+
 def tag(str):
-    return '@'+str
+    return '#'+str
+
+def getMap(geo_lat, geo_long):
+    url = "http://maps.googleapis.com/maps/api/staticmap?center="+str(geo_lat)+","+str(geo_long)+"&size=800x800&zoom=14&sensor=false"
+    buffer = BytesIO(request.urlopen(url).read())
+    Image.open(buffer).save(str(abs(geo_lat))+'.gif','GIF')
 
 def get_api(cfg):
     auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
@@ -19,8 +28,11 @@ def main():
     }
 
     api = get_api(cfg)
-    tweet = "Hello, world!"
-    status = api.update_status(status=tweet) 
+    tweet = "Havana na na na"
+    geo_lat = 1.3552217
+    geo_long = 103.8231561
+    gif = getMap(geo_lat, geo_long)
+    # status = api.update_with_media(filename=gif, status=tweet,lat=geo_lat,long=geo_long)
 
 if __name__ == "__main__":
-  main()
+    main()
