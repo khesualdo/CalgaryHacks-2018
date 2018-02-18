088624909068fe0aeb0688aa36f20c7ac42e3680
00111000 <khesualdo.github@gmail.com>
2018-02-17 17:42:30 -0700

    beta  TweetBot

--- TweetBot.py
+++ TweetBot.py
@@ -1,24 +1,33 @@
 import tweepy
 import KeyParser
+from icecream import ic
 
 from io import BytesIO
 from PIL import Image
 from urllib import request
 
+def id(str):
+    return '@'+str
+
 def tag(str):
     return '#'+str
 
 def getMap(geo_lat, geo_long):
-    url = "http://maps.googleapis.com/maps/api/staticmap?center="+str(geo_lat)+","+str(geo_long)+"&size=800x800&zoom=14&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/staticmap?center="+str(geo_lat)+\
+    ","+str(geo_long)+"&size=800x800&markers=color:red%7Clabel:HAVANA%7C"+str(geo_lat)+\
+    ","+str(geo_long)+"&zoom=14&sensor=false"
+
     buffer = BytesIO(request.urlopen(url).read())
     Image.open(buffer).save(str(abs(geo_lat))+'.gif','GIF')
+
+    return str(abs(geo_lat))+'.gif'
 
 def get_api(cfg):
     auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
     auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
     return tweepy.API(auth)
 
-def main():
+def tweet(tweet=None, geo_lat=None, geo_long=None):
 
     cfg = { 
         "consumer_key"        : KeyParser.consumer_key,
@@ -26,13 +35,24 @@ def main():
         "access_token"        : KeyParser.access_token,
         "access_token_secret" : KeyParser.access_token_secret 
     }
-
     api = get_api(cfg)
-    tweet = "Havana na na na"
-    geo_lat = 1.3552217
-    geo_long = 103.8231561
-    gif = getMap(geo_lat, geo_long)
-    # status = api.update_with_media(filename=gif, status=tweet,lat=geo_lat,long=geo_long)
 
-if __name__ == "__main__":
-    main()
+    if tweet is None:
+        raise Exception('No msg for tweet!')
+
+    if geo_lat is not None and not isinstance(geo_lat, float):
+        raise Exception('Latitude should be a float')
+
+    if geo_long is not None and not isinstance(geo_long, float):
+        raise Exception('Longitude should be a float')
+
+    try:
+        gif = getMap(geo_lat, geo_long)
+        status = api.update_with_media(filename=gif, status=tweet,lat=geo_lat,long=geo_long)
+        # ic(status)
+    except:
+        ic("Error querying GoogleMaps")
+        status = api.update_status(status=tweet)
+        # ic(status)
+
+tweet("asdd", 1.0, 5.0)
